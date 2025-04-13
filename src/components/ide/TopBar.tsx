c08dfac932704093986275f7abf028ddf2dd5f74
Harshkumar2005 <73623311+Harshkumar2005@users.noreply.github.com>
2025-04-13 18:18:22 +0530

    Update TopBar.tsx

--- src/components/ide/TopBar.tsx
+++ src/components/ide/TopBar.tsx
@@ -1,7 +1,7 @@
 //components/ide/TopBar.tsx
 import React, { useState } from 'react';
 import { Command, Save, Settings, File as FileIcon, Edit as EditIcon, Eye, HelpCircle, Copy, Clipboard, Download, Upload, Trash2, Undo, Redo, RotateCcw, X, LayoutGrid, HardDriveDownload, Ghost, Option, Undo2, Redo2, Loader2 } from 'lucide-react';
-import FloppyDisk from "@phosphor-icons/react"
+import FloppyDisk from "@phosphor-icons/react";
 import { useEditor } from '@/contexts/EditorContext';
 import { useTheme } from '@/contexts/ThemeContext';
 import { useFileSystem } from '@/contexts/FileSystemContext';
