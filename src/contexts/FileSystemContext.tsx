a5d1a43a34e2ac61c313b6e014e2f1e3788662fd
Harshkumar2005 <73623311+Harshkumar2005@users.noreply.github.com>
2025-04-14 21:15:42 +0530

    Update FileSystemContext.tsx

--- src/contexts/FileSystemContext.tsx
+++ src/contexts/FileSystemContext.tsx
@@ -137,7 +137,7 @@ const initialFileSystem: FileSystemItem[] = [
          type: 'file',
          path: '/new-project/package.json',
          language: 'json',
-         content: ''{\n  \"name\": \"my-project\",\n  \"version\": \"1.0.0\",\n  \"scripts\": {\n    \"start\": \"vite\"\n  },\n  \"dependencies\": {\n    \"react\": \"^18.0.0\",\n    \"react-dom\": \"^18.0.0\"\n  },\n  \"devDependencies\": {\n    \"vite\": \"^5.0.0\"\n  }\n}',
+         content: '{\n  \"name\": \"my-project\",\n  \"version\": \"1.0.0\",\n  \"scripts\": {\n    \"start\": \"vite\"\n  },\n  \"dependencies\": {\n    \"react\": \"^18.0.0\",\n    \"react-dom\": \"^18.0.0\"\n  },\n  \"devDependencies\": {\n    \"vite\": \"^5.0.0\"\n  }\n}',
          parentId: 'root',
        }]
   }
